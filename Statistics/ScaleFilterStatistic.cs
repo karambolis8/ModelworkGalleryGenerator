3080ed84880d2fc5c809c75564be8e2ddc100d3f
Adam Gudynowski <karambolis8@gmail.com>
2016-10-11 16:05:43 +0200

    implemented post-divide helper for large post

--- Statistics/ScaleFilterStatistic.cs
+++ Statistics/ScaleFilterStatistic.cs
@@ -19,14 +19,28 @@ namespace ModelworkGalleryGenerator.Statistics
 
         public IList<string> GenerateStatisticsRows(IList<GalleryEntry> galleryEntries, string updateDate)
         {
+            var scaleEnties = galleryEntries
+                .Where(e => e.Scales.Contains(this._scale))
+                .OrderBy(e => e.Model);
+            
             var list = new List<string>();
+
+            if (scaleEnties.Count() > 500)
+            {
+                var stat = scaleEnties
+                    .GroupBy(e => e.Title.Trim().ToLower()[0])
+                    .OrderBy(s => s.Key);
+
+                foreach (var s in stat)
+                {
+                    list.Add(string.Format("{0} - {1}", s.Key, s.Count()));
+                }
+            }
 
             var formatStr = this._scale == "nieznana" ? "[size=150][b]Skala {0}[/b][/size]" : "[size=150][b]Skala 1:{0}[/b][/size]";
             list.Add(string.Format(formatStr, this._scale));
 
-            foreach(var entry in galleryEntries
-                .Where(e => e.Scales.Contains(this._scale))
-                .OrderBy(e => e.Model))
+            foreach (var entry in scaleEnties)
             {
                 list.Add(string.Format("[url={0}][b]{1}[/b] {2} ({3})[/url]", entry.URL, entry.Model, entry.Author, string.Join(", ", entry.Producers)));
             }
