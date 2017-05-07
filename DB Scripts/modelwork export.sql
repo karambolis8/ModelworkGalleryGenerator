0e58c4eda690a6cab218de8ce120df16815c5bbb
Adam <karambolis8@gmail.com>
2017-05-07 22:01:41 +0200

    Updated SQL script

--- DB Scripts/modelwork export.sql
+++ DB Scripts/modelwork export.sql
@@ -1,4 +1,5 @@
-SELECT FROM_UNIXTIME(topic_time) 'Data utworzenia', FROM_UNIXTIME(topic_last_post_time) 'Data ostatniego posta', CONCAT('http://modelwork.pl/viewtopic.php?f=', forum_id, '&t=', topic_id) 'URL', topic_title 'Tytu³', topic_first_poster_name 'Autor'
-FROM phpbb3_topics
-WHERE forum_id = 68
-ORDER BY topic_time DESC;
+SELECT FROM_UNIXTIME(topic_time) 'Data utworzenia', FROM_UNIXTIME(topic_last_post_time) 'Data ostatniego posta', CONCAT('http://modelwork.pl/viewtopic.php?f=', forum_id, '&t=', topic_id) 'URL', topic_title 'Tytu³', topic_first_poster_name 'Autor' 
+FROM phpbb3_topics 
+WHERE forum_id = 64 
+AND FROM_UNIXTIME(topic_time) > '2016-09-26 14:43:57' 
+ORDER BY topic_time DESC
